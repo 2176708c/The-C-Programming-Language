a894ba4890be4c78c1412df79ff952b5d88ffe43
2176708c <2176708c@student.gla.ac.uk>
2017-08-31 15:47:59 +0300

    Exercise 2-5 Done

--- Chapter 2/Exercise 2-05.c
+++ Chapter 2/Exercise 2-05.c
@@ -3,7 +3,7 @@
 #define MAXLINE 1000
 
 int getline(char line[], int maxline);
-void squeeze(char s1[], char s2[]);
+int any(char s1[], char s2[]);
 
 int main()
 {
@@ -19,8 +19,7 @@ int main()
     while ((len = getline(s2, MAXLINE)) == 0)
         ;
     
-    squeeze(s1, s2);
-    printf("The result is: %s\n", s1);
+    printf("The first location is at: %d\n", any(s1, s2));
 
     return 0;
 
@@ -32,21 +31,20 @@ int getline(char s[], int lim)
 
     for (i = 0; i<lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
         s[i] = c;
-    if (c == '\n') {
-        s[i++] = c;
-    }   
     s[i] = '\0';
 
     return i;
 }
 
-void squeeze(char s1[], char s2[])
+int any(char s1[], char s2[])
 {
-    int i, j, k;
-    for (i = 0; s2[i] != '\0'; i++){
-        for (j = k = 0; s1[j] != '\0'; j++)
-            if (s1[j] != s2[i]) /* check if s2[i] is in s1*/
-                s1[k++] = s1[j];
-        s1[k] = '\0';
+    int i, j;
+    for (i = 0; s2[i] != '\0'; ++i){
+        for (j = 0; s1[j] != '\0'; ++j){
+            if (s1[j] == s2[i]){ /* check if s2[i] is in s1*/
+                return j;
+            }
+        }
     }
+    return -1;
 }
